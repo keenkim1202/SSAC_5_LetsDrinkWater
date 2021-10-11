b5aa37a284b6aa7029c847aaa98380385dc497bd
KEEN <homurahomu@naver.com>
2021-10-11 14:20:51 +0900

    프로젝트 생성 및 assets 추가

---
+++ LetsDrinkWater/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  LetsDrinkWater
+//
+//  Created by KEEN on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
+
